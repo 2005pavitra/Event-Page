d14cb7f28052d005504fff41644e3edede651bce
Your Name <your_email@example.com>
2025-01-29 00:46:07 +0530

    updated Event name

--- src/Events.jsx
+++ src/Events.jsx
@@ -2,20 +2,10 @@
 export const events = [
     {
       id: 1,
-      title: "Event 1",
-      date: "Feb 2, 2025",
+      title: "RISE Startup Pitch",
+      date: "31 Jan 2025",
       description: "Exciting upcoming event!",
     },
-    {
-      id: 2,
-      title: "Event 2",
-      date: "Jan 20, 2025",
-      description: "Exciting upcoming event!",
-    },
-    {
-      id: 3,
-      title: "Event 3",
-      date: "Jan 31, 2025",
-      description: "Exciting upcoming event!",
-    },
+    
+
   ];
